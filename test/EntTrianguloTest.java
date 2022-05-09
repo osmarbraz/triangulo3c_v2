056d8a5a124bae2638bd6063352a3dc3bdc2f0a3
admin <admin@OSMARVM0>
2022-05-09 07:29:41 -0300

    Atualização dos testes.

--- test/EntTrianguloTest.java
+++ test/EntTrianguloTest.java
@@ -1,7 +1,7 @@
 
-
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class EntTrianguloTest {
     
@@ -14,5 +14,11 @@ public class EntTrianguloTest {
         double resultadoEsperado = 3.0;
         double resulto = instancia.getArea();
         assertEquals(resultadoEsperado, resulto, 0);             
-    }       
+    }
+    
+    @Test
+    public void testEntTriangulo() {
+        EntTriangulo instancia = new EntTriangulo();
+        assertTrue((instancia.getAltura()==0 && instancia.getBase() == 0));
+    }
 }
